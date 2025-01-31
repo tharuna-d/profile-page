b716d8e89b45c6bd305d472d66de551716f715bb
Tharuna <tharuna.d2023ai-ds@sece.ac.in>
2025-01-31 10:42:04 +0530

    first time

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<ProfilePage />} />
+        <Route path="/profile-page" element={<ProfilePage />} />
       </Routes>
     </Router>
   );
